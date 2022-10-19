c6d510564641271219d3d745c30ef0b3aeb9110c
shenlian <shenlian6@huawei.com>
2022-10-19 09:40:45 +0800

    install.sh

--- aiguard_plugin/install.sh
+++ aiguard_plugin/install.sh
@@ -3,7 +3,7 @@ set -e
 CUR_DIR=$(cd $(dirname $0);cd ..; pwd)
 function run_service(){
     mkdir -p /var/lib/kubelet/seccomp/profiles
-    cp -r ${CUR_DIR}/aiguard_plugin /usr/local/bin/
+    cp -rf ${CUR_DIR}/aiguard_plugin /usr/local/bin/
     cp ${CUR_DIR}/aiguard_plugin/service/aiguard_plugin.service /etc/systemd/system
     dos2unix /usr/local/bin/aiguard_plugin/limit_file/cfs_profile || exit 1
     apparmor_parser -r -W /usr/local/bin/aiguard_plugin/limit_file/cfs_profile || exit 2
