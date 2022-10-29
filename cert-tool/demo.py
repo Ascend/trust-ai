9ebacdd19b4f6ce7cb9e2835e4b18c338b2179d6
nauyll <ly_luo1204@163.com>
2022-10-29 14:58:14 +0800

    add: cert tool

--- cert-tool/demo.py
+++ cert-tool/demo.py
@@ -5,9 +5,11 @@ from ssl_key import SSLKey
 
 if __name__ == "__main__":
     demo = SSLKey(alg_id=1)
+    # encrypt test
     pwd = "abcdefghijklmnopqrstuvwxyz1234567890qwertyuiop"
     demo.generate(pwd, file_path="./")
 
+    # decrypt test
     with open("./server.key", "rb") as kf:
         cipher_data = kf.read()
     plain_private_key = demo.parse_cipher_data(cipher_data, pwd)
