4fed2db35ce6544472430c61599a0e1f8496ffe8
yuxiang <wangyuxiang29@huawei.com>
2022-12-08 21:26:09 +0800

    init logger

--- ai-vault/cert-manager/run.py
+++ ai-vault/cert-manager/run.py
@@ -1,11 +1,13 @@
 # coding: UTF-8
 # Copyright (c) 2022. Huawei Technologies Co., Ltd. ALL rights reserved.
 from gevent import pywsgi
-from main.app import app, register_api
+from main.app import app, register_api, init_logger
 from config import RUN_LOG, LOG_INFO
+
 
 if __name__ == "__main__":
     register_api()
+    init_logger()
     RUN_LOG.log(LOG_INFO, "Start user-manager service success.")
     server = pywsgi.WSGIServer(("0.0.0.0", 10001), app)
     server.serve_forever()
