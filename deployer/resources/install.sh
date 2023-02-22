476979ffca357642308053f2aae336bf80b67504
swagglian <1312140415@qq.com>
2023-02-22 08:06:24 +0000

    update deployer/resources/install.sh.
    Signed-off-by: swagglian <1312140415@qq.com>

--- deployer/resources/install.sh
+++ deployer/resources/install.sh
@@ -21,7 +21,7 @@ function run_docker() {
   [ $(docker ps -a|grep ai-vault-svc|wc -l) -ne 0 ] && docker stop ai-vault-svc && docker rm ai-vault-svc
   docker run -d --restart=always -p $mgmt_port:9000 -p $svc_port:5001 --name ai-vault-svc -v /home/AiVault/.ai-vault:/home/AiVault/.ai-vault -e  AIVAULT_ARGS=$aivault_args $image bash -c /home/AiVault/run.sh
   sleep 3
-  docker ps | grep $image | grep "Up"
+  docker ps | grep $image | egrep "Up [0-9]{1,2} seconds"
   if [ $? -eq 0 ]; then
     echo "Install successful. Please visit https://ip:$mgmt_port/"
     exit 0
